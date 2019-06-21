a61c5c7a43a15f68e9df76a4622ee05f3c2d3740
Ninfa Niño <ninfa_sys@hotmail.com>
2019-06-20 21:13:51 -0500

    Se crearon rutas Home, Dashboard y Payment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
+import Home from './pages/home'
+import Dashboard from './pages/dashboard'
+import Payment from './pages/payment'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Route path="/" exact component={Home} />
+        <Route path="/dashboard/" component={Dashboard} />
+        <Route path="/payment/" component={Payment} />
+      </div>
+    </Router>
   );
 }
 

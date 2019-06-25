de252a85e6681c969a1b298ac40fbe42857fde45
Ninfa Niño <ninfa_sys@hotmail.com>
2019-06-24 22:54:38 -0500

    componente perros encontrados

---
+++ src/components/home/perros-encontrados.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+/****
+ * Necesita conectarse al back
+ ***/
+
+const PerrosEncontrados = () => (
+    <div className="PerrosEncontrados">
+        <div className="title">
+            perros encontrados
+        </div>
+        <div className="lineGreen"></div>
+        <div className="slogan">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </div>
+
+        <div className="PerrosEncontradosContainer">
+            <div className="PerroEncontradoItem">
+                <img src="/images/slidder1.jpg" className="PerroEncontradoImg" />
+                <div className="mask">
+                    <img src="/images/icon-location.png" className="iconLocation" />
+                    <div className="addressLocation">
+                        Zapopan, Jal <br/>3311771791
+                    </div>
+                </div>
+            </div>
+
+            <div className="PerroEncontradoItem">
+                <img src="/images/slidder2.jpg" className="PerroEncontradoImg" />
+                <div className="mask">
+                    <img src="/images/icon-location.png" className="iconLocation" />
+                    <div className="addressLocation">
+                        Zapopan, Jal <br/>3311771791
+                    </div>
+                </div>
+            </div>
+
+            <div className="PerroEncontradoItem">
+                <img src="/images/slidder3.jpg" className="PerroEncontradoImg" />
+                <div className="mask">
+                    <img src="/images/icon-location.png" className="iconLocation" />
+                    <div className="addressLocation">
+                        Zapopan, Jal <br/>3311771791
+                    </div>
+                </div>
+            </div>
+
+            <div className="PerroEncontradoItem">
+                <img src="/images/slidder4.jpg" className="PerroEncontradoImg" />
+                <div className="mask">
+                    <img src="/images/icon-location.png" className="iconLocation" />
+                    <div className="addressLocation">
+                        Zapopan, Jal <br/>3311771791
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default PerrosEncontrados
--- src/pages/home.js
+++ src/pages/home.js
@@ -3,6 +3,7 @@ import Header from '../components/home/header';
 import '../components/fonts.css'
 import '../components/home/home.css'
 import Events from '../components/home/events';
+import PerrosEncontrados from '../components/home/perros-encontrados';
 
 /*****
  * Header necesita enviar una funcion para abrir el formulario de registro
@@ -11,7 +12,9 @@ import Events from '../components/home/events';
 const Home = () => (
     <div>
         <Header/> 
+        <PerrosEncontrados/>
         <Events/>
+        
     </div>
 )
 

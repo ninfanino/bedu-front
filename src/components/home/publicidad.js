7eb361230f064a3682a5563630b9c06efedcadb4
Ninfa Niño <ninfa_sys@hotmail.com>
2019-06-25 12:58:12 -0500

    componente publicidad

---
+++ src/components/home/publicidad.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Publicidad = () => (
+    <div class="Publicidad">
+        <div class="item1"></div>
+        <div class="item2"></div>
+        <div class="item3"></div> 
+    </div>
+)
+
+export default Publicidad;
--- src/pages/home.js
+++ src/pages/home.js
@@ -6,6 +6,7 @@ import Events from '../components/home/events';
 import PerrosEncontrados from '../components/home/perros-encontrados';
 import Adopciones from '../components/home/adopciones';
 import Blog from '../components/home/blog';
+import Publicidad from '../components/home/publicidad';
 
 /*****
  * Header necesita enviar una funcion para abrir el formulario de registro
@@ -18,6 +19,7 @@ const Home = () => (
         <Adopciones/>
         <Events/>
         <Blog/>
+        <Publicidad/> 
     </div>
 )
 

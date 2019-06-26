343a4798db43b58983417327a62595371f6fc5b0
alexanderlonvii <alexanderleonvii@gmail.com>
2019-06-25 21:22:18 -0500

    Agregando el menu

---
+++ src/components/home/Menu.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Icon from './Icon'
+import {Link, NavLink} from 'react-router-dom';
+
+
+const Menu = () => (
+  <div className="Menu">
+    <div className="MenuLogo">
+      <img src="./images/logo-header.png" alt="XUXO" id="MenuImage"></img>
+    </div>
+    <div className="MenuNav">
+      <NavLink to="/" activeClassName="isSelected" className="MenuOptions">EMERGENCIAS</NavLink>
+      <NavLink to="/" activeClassName="isSelected" className="MenuOptions">PERROS ENCONTRADOS</NavLink>
+      <NavLink to="/" activeClassName="isSelected" className="MenuOptions">EVENTOS</NavLink>
+      <NavLink to="/" activeClassName="isSelected" className="MenuOptions">DONAR</NavLink>
+      <NavLink to="/" activeClassName="isSelected" className="MenuIcons">
+        <img src="./images/icon-fb.png" alt="FB" ></img>
+      </NavLink>
+      <NavLink to="/" activeClassName="isSelected" className="MenuIcons">
+        <img src="./images/icon-in.png" alt="Instagram" ></img>
+      </NavLink>
+      <NavLink to="/" activeClassName="isSelected" className="MenuIcons">
+        <img src="./images/icon-account.png" alt="Account" ></img>
+      </NavLink>
+    </div>
+  </div>
+)
+
+export default Menu
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Menu from '../components/home//Menu'
 import Header from '../components/home/header';
 import '../components/fonts.css'
 import '../components/home/home.css'
@@ -9,12 +10,14 @@ import Blog from '../components/home/blog';
 import Publicidad from '../components/home/publicidad';
 import Patrocinadores from '../components/home/patrocinadores';
 
+
 /*****
  * Header necesita enviar una funcion para abrir el formulario de registro
 *****/
 
 const Home = () => (
     <div>
+        <Menu/>
         <Header/> 
         <PerrosEncontrados/>
         <Adopciones/>

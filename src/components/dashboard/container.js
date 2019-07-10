f72f33a966b75a0f2cde807f62c95e2a2b3050f9
Ninfa Niño <ninfa_sys@hotmail.com>
2019-07-09 20:04:56 -0500

    Dashboard: Mis mascotas

---
+++ src/components/dashboard/container.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import MyPets from './myPets';
+
+const Container = () => (
+    <div className="Container">
+        <MyPets/>
+    </div>
+)
+
+export default Container
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import MenuDashboard from '../components/dashboard/menu';
+
+import '../components/dashboard/dashboard.css'
+import Container from '../components/dashboard/container';
+import Ads from '../components/dashboard/ads';
 
 const Dashboard = () => (
-    <div>Dashboard</div>
+    <div className="Dashboard">
+        <MenuDashboard/>
+        <Container/>
+        <Ads/>
+    </div>
 )
 
 export default Dashboard
